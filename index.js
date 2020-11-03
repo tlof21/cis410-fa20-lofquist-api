480dc9b18ed73f0d541b5ff05de0a169b5968cee
tlof21 <magnet3@live.com>
2020-11-03 15:11:56 -0700

    10.1 10/27 /movies/:pk

--- index.js
+++ index.js
@@ -23,4 +23,27 @@ app.get("/movies", (req,res)=>{
     })
 })
 
+app.get("/movies/:pk", (req, res)=>{
+    var pk = req.params.pk
+    //console.log("my PK:", pk)
+
+    var myQuery = `SELECT * 
+    FROM movie
+    LEFT JOIN genre
+    ON genre.GenrePK = Movie.GenreFK
+    WHERE moviePK = ${pk}`
+
+    db.executeQuery(myQuery)
+    .then((movies)=>{
+        //console.log("Movies: ", movies)
+
+        if(movies[0]){
+            res.send(movies[0])
+        }else{res.status(404).send('bad request')}
+    })
+    .catch((err)=>{
+        console.log("Error in /movies/pk", err)
+        res.status(500).send()
+    })
+})
 app.listen(5000,()=>{console.log("app is running on port 5000")})
